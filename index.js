2d1f041819ee067a619479eda63570ea65d910b6
Gary Flugel <flugelg@gmail.com>
2021-11-29 03:58:08 -0500

    commit tree 58748c6fa476e193583bb5222acb840b0c900a2d

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+let multiply;
 
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+
+multiply = num1 * num2;
+
+let random = Math.floor(Math.random()) + 1;
+
+let mod;
+let num3 = 19
+let num4 = 5
+
+mod = num3 % num4;
+
+let max = Math.max(0, 9, 12, 20);
+
+
+
+
